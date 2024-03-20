5e3057bd481712b1b58dcf0d771daad8675a91b3
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-20 18:15:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`
+
+const mondayWork = (activity = "go to the office") => `This Monday, I will ${activity}.`
+
+module.exports = { saturdayFun, mondayWork }
+
+function wrapAdjective(symbol = '*') {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
+  
+  module.exports = {
+    wrapAdjective,
+  };
+  
